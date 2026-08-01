6f73e44848bbc36006ecbba996401355da1a344f
Frank Li <55543424+yzFrankli@users.noreply.github.com>
2026-07-31 20:23:52 -0400

    deleted .next

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"EH4KC+yCGvR9ZoXNYOz1rhXf0Ll7Ee+G9cZpaI9SG3w=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_1-7dtlk._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_1qdccu3._.js","static/chunks/_17l6_ks._.js"],
-    source: "dynamic"
-});
