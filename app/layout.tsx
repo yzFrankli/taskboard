d03c643c700608b869f02a80feb353249377779e
Frank Li <55543424+yzFrankli@users.noreply.github.com>
2026-08-19 20:00:43 -0400

    made changes to layout and added visitor analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans, Syne } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
